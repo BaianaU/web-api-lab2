8a72562446734747f38bc2e6de883ee7a28ab17b
BaianaU <baianausupbaeva@gmail.com>
2025-01-23 13:22:03 +0100

    create custom query to retrieve user from DB by username and password

--- src/main/kotlin/com/BaianaU/web_api_lab2/repository/CustomUserRepository.kt
+++ src/main/kotlin/com/BaianaU/web_api_lab2/repository/CustomUserRepository.kt
@@ -2,9 +2,11 @@ package com.BaianaU.web_api_lab2.repository
 
 import com.BaianaU.web_api_lab2.model.CustomUser
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface CustomUserRepository: JpaRepository <CustomUser, Long>{
+    @Query("SELECT u FROM CustomUser u WHERE u.username = :username AND u.password = :password")
     fun findByNameAndPassword(@Param("username") username: String,
                               @Param("password") password: String): CustomUser
 }
