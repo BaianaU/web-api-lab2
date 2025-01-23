0dc033623041fbc4cfcd511af48e5b300bf0f91a
BaianaU <baianausupbaeva@gmail.com>
2025-01-23 20:26:09 +0100

    Implementerat BCryptPasswordEncoder i CustomUserController

--- src/main/kotlin/com/BaianaU/web_api_lab2/controller/CustomUserController.kt
+++ src/main/kotlin/com/BaianaU/web_api_lab2/controller/CustomUserController.kt
@@ -4,6 +4,7 @@ import com.BaianaU.web_api_lab2.model.user.CustomUser
 import com.BaianaU.web_api_lab2.repository.CustomUserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/user")
 class CustomUserController @Autowired constructor(
-    val customUserRepository: CustomUserRepository
+    val customUserRepository: CustomUserRepository,
+    val passwordEncoder: PasswordEncoder
 ) {
+
+    @GetMapping("/password")
+    fun getBcryptPassword(): String {
+
+        val testPassword = "123"
+
+        return passwordEncoder.encode(testPassword)
+
+    }
 
     @GetMapping
     fun getUser(name: String, password: String): ResponseEntity<CustomUser> {
@@ -26,6 +37,12 @@ class CustomUserController @Autowired constructor(
     fun saveUser(
         @RequestBody newUser: CustomUser
     ): ResponseEntity<String> {
+
+        val bcryptUser = CustomUser(
+            newUser.username,
+            passwordEncoder.encode(newUser.password)
+
+        )
 
         customUserRepository.save(newUser)
         return ResponseEntity.status(201).body("User was successfully created")
